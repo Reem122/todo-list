fb762ad20298aa11c396048d7a85efca90bb410f
Reem122 <73790359+Reem122@users.noreply.github.com>
2021-07-17 09:33:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//btn is not disabled when input is entered  
+function btnNotDisabled(){
+  var input = document.getElementById("input").value;
+    var submit = document.getElementById("submit");
+    submit.classList.remove("disabled");
+    if(input == ""){
+      submit.classList.add("disabled")
+    }
+  
+}
+
+// add task
+function addTask(){
+  var input = document.getElementById("input").value;
+  var submit = document.getElementById("submit");
+  var tasks = document.getElementById("tasks");
+  var task = document.createElement("li");
+  task.classList.add("task", "position-relative");
+  tasks.appendChild(task);
+  task.innerHTML = "<input type=\"checkbox\" class=\"checkbox form-check-input mb-2 mb-md-0\"> <p>" + input + "</p> <i onclick=\"this.parentElement.remove();\" class=\"bi bi-x-lg position-absolute\"></i>";
+  document.getElementById("input").value = "";
+  submit.classList.add("disabled");
+}
